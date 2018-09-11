79a2127d1318fe62686430d0311d512e446cc0ab
Avahe Kellenberger <avahe.k@gmail.com>
2018-09-11 19:51:11 -0400

    Fixed a bug where leading/trailing whitespace would cause curl to fail uploading the file.

--- tsh.sh
+++ tsh.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-file="${1}"
+# Removes leading and trailing whitespace.
+trim() {
+    local var="$*"
+    var="${var#"${var%%[![:space:]]*}"}"
+    var="${var%"${var##*[![:space:]]}"}"
+    echo -n "$var"
+}
+
+file=$(trim "$1")
 file_name="${file##*/}"
 printf "%s\r\n" "$(curl --upload-file "${file}" "https://transfer.sh/${file_name}")"
